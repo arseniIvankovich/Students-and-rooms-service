b6ec6422a00e6dc29b643f7cac2ba541021dc16a
arseniIvankovich <arseniivankovich0@gmail.com>
2024-04-30 13:32:07 +0200

    git commit -m "PYT-4943 add logging"

--- connection.py
+++ connection.py
@@ -1,14 +1,19 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
+import logging
+import logging.config
 
 load_dotenv()
+logging.config.fileConfig('logging.conf', disable_existing_loggers=True, encoding=None)
+connection_logger = logging.getLogger('connection_logger')
+
 
 def connect():
     try:
-        with psycopg2.connect(database=os.getenv("DATABASE_NAME"), user=os.getenv("DATABASE_USER"), 
-                              password=os.getenv("DATABASE_PASSWORD"), host=os.getenv("DATABASE_HOST")) as connection:
-            print('Connect')
+        with psycopg2.connect(database=os.getenv("DATABASE_NAME"), user= os.getenv("DATABASE_USER"), 
+                                password=os.getenv("DATABASE_PASSWORD"), host=os.getenv("DATABASE_HOST")) as connection:
             return connection
     except (psycopg2.DatabaseError, Exception) as e:
-        print(e)
+        connection_logger.error(e)
+            
